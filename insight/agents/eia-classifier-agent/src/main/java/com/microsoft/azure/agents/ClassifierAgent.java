2c8cce7aca6c8598b7a7661deb4f27ad3a718b04
Tirthankar Barari <tirtho@hotmail.com>
2026-05-15 09:04:31 -0400

    basic project structure for agents added

---
+++ insight/agents/eia-classifier-agent/src/main/java/com/microsoft/azure/agents/ClassifierAgent.java
@@ -0,0 +1,9 @@
+package com.microsoft.azure.agents;
+
+public class ClassifierAgent {
+    private static final Logger logger = Logger.getLogger(ClassifierAgent.class.getName());
+
+    public static void main(String[] args) {
+
+    }
+}
